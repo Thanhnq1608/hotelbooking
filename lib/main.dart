c10d2c6707fdcd0c769b1aad0848fcf4f5a9cac4
VietTienNguyen <viettiennguyen2607@gmail.com>
2021-11-22 19:06:15 +0700

    feat: add screen history fix: bottomnavigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:hotelbooking/app.dart';
 import 'package:hotelbooking/routes/app_pages.dart';
 
 void main() async {
@@ -11,6 +12,7 @@ void main() async {
         backgroundColor: Color(0xFFF8F0F0), primaryColor: Color(0xFFFF6666)),
     debugShowCheckedModeBanner: false,
     initialRoute: AppPage.initial,
-    getPages: AppPage.pages,
+    //  getPages: AppPage.pages,
+    home: App(),
   ));
 }
